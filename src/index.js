b1c6c5e152d9b3b49e0a8a6897c2205664a651e1
Sonny Lazuardi Hermawan <sonnylazuardi@gmail.com>
2018-05-18 20:38:27 +0800

    test

--- src/index.js
+++ src/index.js
@@ -63,4 +63,41 @@ app.post("/read", (req, res) => {
   });
 });
 
+app.post("/readhd", (req, res) => {
+  const { queryResult } = req.body;
+  const { buku, pasal } = queryResult.parameters;
+  const activeBook = Books.find(book => book.value == buku);
+  let bookId = Books.findIndex(book => book.value == activeBook.value);
+  if (activeBook.type == "new") {
+    bookId = bookId - 39;
+  }
+  const bookNumber = pad(bookId + 1, 2);
+  let bookName = activeBook.name_id.toLowerCase();
+  bookName = bookName
+    .replace("1 ", "1")
+    .replace("2 ", "2")
+    .replace("3 ", "3");
+  if (bookName.indexOf(" " != -1)) {
+    let bookSplit = bookName.split(" ");
+    bookName = bookSplit[0];
+  }
+  if (bookName.indexOf("-" != -1)) {
+    let bookSplit = bookName.split("-");
+    bookName = bookSplit[0];
+  }
+  let bookShortName = activeBook.value_id.toLowerCase();
+
+  const testament = activeBook.type == "old" ? "pl" : "pb";
+  let chapterNumber = activeBook.total > 99 ? pad(pasal, 3) : pad(pasal, 2);
+  const streamUrl = `https://acakata.getputik.com/audio?url=alkitab_audio/tb_alkitabsuara/${testament}/mp3/cd/${bookNumber}_${bookName}/${bookNumber}_${bookShortName}${chapterNumber}.mp3`;
+  const fulfillmentText = `<speak>
+  Here is ${activeBook.name} chapter ${pasal}...
+  <audio src="${streamUrl}" />
+  </speak>`;
+  console.log(fulfillmentText);
+  res.json({
+    fulfillmentText: fulfillmentText
+  });
+});
+
 app.listen(PORT, () => console.log("Example app listening on port 3000!"));
